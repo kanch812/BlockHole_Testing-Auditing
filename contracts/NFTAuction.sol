0df9b012380a1a8e75d8a288c59e0f8c1bfa91e2
Kanchan Khatri <812.kanchankhatri@gmail.com>
2022-12-27 22:58:18 +0530

    Updating auction contract

--- contracts/NFTAuction.sol
+++ contracts/NFTAuction.sol
@@ -1,88 +1,147 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.4;
 
-interface IERC721 {
-  function safeTransferFrom(address from, address to, uint tokenId) external;
+import "./NFTMarketplace.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-  function transferFrom(address, address, uint) external;
-}
+contract NFTAuction {
+    NFTMarketplace marketplace;
+    address marketplaceAddress;
+    address NFTMarketplaceOwner;
+    mapping(uint256 => Auction) public IdtoAuction; // tokenid to auction
+    mapping(uint256 => mapping(address => uint256)) public bids; // tokenid to bids of addresses
+    uint256 listingFee = 0.01 ether;
 
-contract EnglishAuction {
-  // events
-  event Start();
-  event Bid(address indexed sender, uint amount);
-  event Withdraw(address indexed bidder, uint amount);
-  event End(address winner, uint amount);
-
-  IERC721 public nft;
-  uint public nftId;
-
-  address payable public seller;
-  uint public endAt; //0
-  bool public started; // default value false
-  bool public ended; //default value false
-
-  address public highestBidder; //0*00000
-  uint public highestBid; // 0
-
-  //address is mapped to bid amount
-  mapping(address => uint) public bids;
-
-  // constructor takes nft token address, token id, starting bitd amount
-  constructor(address _nft, uint _nftId, uint _startingBid) {
-    nft = IERC721(_nft); // address
-    nftId = _nftId; //id
-
-    seller = payable(msg.sender); //seller who owns the NFT
-    highestBid = _startingBid; // assigning highest bid
-  }
-
-  // function to start the auction
-  function start() external {
-    require(!started, "started"); // started = false
-    require(msg.sender == seller, "not seller");
-
-    nft.transferFrom(msg.sender, address(this), nftId);
-    started = true;
-    endAt = block.timestamp + 7 days;
-
-    emit Start();
-  }
-
-  function bid() external payable {
-    require(started, "not started");
-    require(block.timestamp < endAt, "ended");
-    require(msg.value > highestBid, "value < highest");
-
-    if (highestBidder != address(0)) {
-      bids[highestBidder] += highestBid;
-    }
-    highestBidder = msg.sender;
-    highestBid = msg.value;
-    emit Bid(msg.sender, msg.value);
-  }
-
-  function withdraw() external {
-    uint bal = bids[msg.sender];
-    bids[msg.sender] = 0;
-    payable(msg.sender).transfer(bal);
-
-    emit Withdraw(msg.sender, bal);
-  }
-
-  function end() external {
-    require(started, "not started");
-    require(block.timestamp >= endAt, "not ended");
-    require(!ended, "ended");
-
-    ended = true;
-    if (highestBidder != address(0)) {
-      nft.safeTransferFrom(address(this), highestBidder, nftId);
-      seller.transfer(highestBid);
-    } else {
-      nft.safeTransferFrom(address(this), seller, nftId);
+    // need to get some details from imported nftmarketplace contract
+    struct Auction {
+        // address marketplaceAddress;
+        uint nftId;
+        address payable seller;
+        uint minPrice;
+        uint endAt;
+        bool started;
+        bool ended;
+        address highestBidder;
+        uint highestBid;
+        address creator;
+        uint royaltPercent;
     }
 
-    emit End(highestBidder, highestBid);
-  }
+    constructor(address _marketplaceAddress, address _marketplaceOwner) {
+        marketplace = NFTMarketplace(_marketplaceAddress);
+        marketplaceAddress = _marketplaceAddress;
+        NFTMarketplaceOwner = payable(_marketplaceOwner);
+    }
+
+    modifier onlyOwner() {
+        require(
+            msg.sender == NFTMarketplaceOwner,
+            "only owner of the marketplace can perform this action"
+        );
+        _;
+    }
+
+    // TODO
+    // for starting auction the seller should pay some listing commision.
+
+    function start(
+        uint nftId,
+        uint _minPrice,
+        uint8 auctiondays
+    ) external payable {
+        require(!IdtoAuction[nftId].started, "Started");
+        require(
+            msg.sender == IERC721(marketplaceAddress).ownerOf(nftId),
+            "Started"
+        );
+        require(msg.value == listingFee, "Must be equal to listing price");
+        require(
+            auctiondays <= 7 && auctiondays >= 1,
+            "auction time should be less than 7 days and more than 1 day"
+        );
+        // the seller should approve this contract to execute the below code
+        // the approval function can be put in front-end
+
+        IdtoAuction[nftId].started = true;
+        IdtoAuction[nftId].nftId = nftId;
+        IdtoAuction[nftId].seller = payable(msg.sender);
+        IdtoAuction[nftId].minPrice = _minPrice;
+        IdtoAuction[nftId].endAt = block.timestamp + auctiondays;
+
+        IdtoAuction[nftId].creator = marketplace.fetchCreatorNft(nftId);
+
+        IdtoAuction[nftId].royaltPercent = marketplace.fetchRoyaltyPercentofNft(
+            nftId
+        );
+
+        IERC721(marketplaceAddress).transferFrom(
+            msg.sender,
+            address(this),
+            nftId
+        );
+
+        // emit start();
+    }
+
+    function updatelistingFee(uint256 _listingFee) external onlyOwner {
+        listingFee = _listingFee;
+
+        // emit ListingChargeUpdated();
+    }
+
+    function withdrawCommission() external onlyOwner {
+        uint256 balance = address(this).balance;
+        require(balance > 0, "Zero balance in the account.");
+        payable(NFTMarketplaceOwner).transfer(address(this).balance);
+
+        // emit MarketplaceBalanceWithdrew();
+    }
+
+    function bid(uint nftId) external payable {
+        require(IdtoAuction[nftId].started, "Not Started");
+        require(block.timestamp < IdtoAuction[nftId].endAt, "ended");
+        require(
+            msg.value > IdtoAuction[nftId].highestBid, /*msg.value + IdtoAuction[nftId].bids[msg.sender]*/
+            "value should be greater than current highest bid"
+        );
+
+        if (IdtoAuction[nftId].highestBidder != address(0)) {
+            bids[nftId][IdtoAuction[nftId].highestBidder] += IdtoAuction[nftId]
+                .highestBid; /*+=msg.value*/
+        }
+
+        IdtoAuction[nftId].highestBidder = msg.sender;
+        IdtoAuction[nftId].highestBid = msg.value; /*msg.value + IdtoAuction[nftId].bids[msg.sender]*/
+
+        // emit Bid();
+    }
+
+    function withdraw(uint nftId) external {
+        require(block.timestamp > IdtoAuction[nftId].endAt);
+        uint bal = bids[nftId][msg.sender];
+        bids[nftId][msg.sender] = 0;
+        payable(msg.sender).transfer(bal);
+        // emit Withdraw();
+    }
+
+    function end(uint nftId) external {
+        require(IdtoAuction[nftId].started, "not started");
+        require(block.timestamp > IdtoAuction[nftId].endAt);
+        require(!IdtoAuction[nftId].ended, "ended");
+        IdtoAuction[nftId].ended = true;
+
+        // need to send nft to the buyer, need to transfer royalty to creator
+    }
+
+    function fetchNftAuctionData(uint nftId)
+        public
+        view
+        returns (Auction memory)
+    {
+        return IdtoAuction[nftId];
+    }
+
+    function fetchMyBidAmountDataForNft(uint nftId) public view returns (uint) {
+        return bids[nftId][msg.sender];
+    }
 }
